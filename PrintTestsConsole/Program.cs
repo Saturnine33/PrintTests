8aa415eb7fb19f2c668c9bddc281084697f1417b
eric_finch_33 <eric_finch_33@yahoo.com>
2023-10-19 21:09:57 -0400

    start method

--- PrintTestsConsole/Program.cs
+++ PrintTestsConsole/Program.cs
@@ -6,7 +6,7 @@ namespace PrintTestsConsole
     {
         static void Main(string[] args)
         {
-            PrintTests.Business.PrintMethods.PrintMethodInnerLoop();
+            PrintTests.Business.PrintMethods.PrintMethodHello();
         }
     }
 }
