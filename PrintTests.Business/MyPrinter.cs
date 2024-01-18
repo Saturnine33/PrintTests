308c5b6f94aff3c58f0f0b914874f06a17361c47
eric_finch_33 <eric_finch_33@yahoo.com>
2024-01-17 19:02:33 -0500

    test

--- PrintTests.Business/MyPrinter.cs
+++ PrintTests.Business/MyPrinter.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿using PrintTests.Business.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace PrintTests.Business
 {
-    public class MyPrinter
+    public class MyPrinter : ITestPrinter
     {
+        readonly IPrinterTarget PrintTarget;
+
+        public MyPrinter(IPrinterTarget target) {
+            PrintTarget = target;
+        }
+
+        public void PrintHelloWorld()
+        {
+            PrintTarget.Print("Hello World");
+        }
     }
 }
